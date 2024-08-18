a6beb28dd7f00e83e29326dda484c5ac42f24a0b
bregarulai <bregarulai@gmail.com>
2024-08-17 21:19:26 -0400

    implement butons to preview resume, accept or reject candidate added

--- features/jobs/components/applicants/ApplicantDataTable.tsx
+++ features/jobs/components/applicants/ApplicantDataTable.tsx
@@ -56,16 +56,28 @@ export function ApplicantDataTable<TData, TValue>({
 
   return (
     <div>
-      <div className="flex items-center py-4">
-        <Input
-          placeholder="Filter names..."
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
+      <div className="flex items-center justify-between">
+        <div className="flex items-center py-4">
+          <Input
+            placeholder="Filter names..."
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("name")?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm"
+          />
+        </div>
+        <div>
+          <Button>Preview Resume</Button>
+        </div>
+        <div>
+          <div className="flex gap-4">
+            <Button>Accept</Button>
+            <Button variant="outline">Reject</Button>
+          </div>
+        </div>
       </div>
+
       <div className="rounded-md border">
         <Table>
           <TableHeader>
