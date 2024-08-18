8a77255e5833d134bda81ac6cd30efea1a23bc78
bregarulai <bregarulai@gmail.com>
2024-08-18 11:02:34 -0400

    implement activities page

---
+++ app/(root)/activities/page.tsx
@@ -0,0 +1,13 @@
+const ActivitiesPage = () => {
+  return (
+    <div className="mx-auto max-w-7xl">
+      <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-800">
+          Activities
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default ActivitiesPage;
